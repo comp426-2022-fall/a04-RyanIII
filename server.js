9e6befa9595f96f42a18427a14a12a04422b57fa
Ryan Nguyen <69874455+RyanIII@users.noreply.github.com>
2022-12-02 17:14:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+#!/usr/bin/env node
 
+import minimist from "minimist";
+import express from "express";
+import {roll} from "./lib/roll.js";
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
+
+const app = express();
+app.use(express.urlencoded({extended: true})));
