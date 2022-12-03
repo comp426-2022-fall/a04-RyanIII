ba0bfb1ac62549e38555b51a37072709d8aad845
Ryan Nguyen <69874455+RyanIII@users.noreply.github.com>
2022-12-02 22:05:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -22,11 +22,36 @@ app.get("/app/roll", (req, res) => {
 
 // part 5
 app.post("/app/roll/" (req, res) => {
-  const sides = parseInt(req.body.sides)
-  const dice = parseInt(req.body.dice)
-  const rolls = parseInt(req.body.rolls)
-  
-  res.send(roll(sides, dice, rolls))
+  const sides = parseInt(req.body.sides);
+  const dice = parseInt(req.body.dice);
+  const rolls = parseInt(req.body.rolls);
+  res.send(roll(sides, dice, rolls));
 });
 
 // part 6
+app.get("/app/roll/:sides/", (req, res) => {
+  const sides = parseInt(req.params.sides);
+  res.send(roll(sides, 2, 1));
+});
+
+// part 7
+app.get("/app/roll/:sides/:dice", (req, res) => {
+  const sides = parseInt(req.params.sides);
+  const dice = parseInt(req.params.dice);
+  res.send(roll(sides, dice, 1));
+});
+
+// part 8
+app.get("/app/roll/:sides/:dice/:rolls", (req, res) => {
+  const sides = parseInt(req.params.sides);
+  const dice = parseInt(req.params.dice);
+  const rolls = parseInt(req.params.rolls);
+  res.send(roll(sides, dice, rolls));
+});
+
+// 404 NOT found
+app.use((req, res) => {
+  res.status(404).send("404 NOT FOUND")
+});
+
+app.listen(args.port || 5000);
