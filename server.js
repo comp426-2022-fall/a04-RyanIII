79f5bdca9c09372948862b5a8b9b312c3801d0ff
Ryan Nguyen <69874455+RyanIII@users.noreply.github.com>
2022-12-02 20:05:41 -0500

    Update server.js
    up to part 5

--- server.js
+++ server.js
@@ -9,3 +9,24 @@ const port = args.port || 5000;
 
 const app = express();
 app.use(express.urlencoded({extended: true})));
+
+// checks endpoint adn returns 200 OK (part 3)
+app.get("/app/", (req, res) => {
+  res.send('200 OK');
+});
+
+// part 4
+app.get("/app/roll", (req, res) => {
+  res.send(roll(6, 2, 1));
+});
+
+// part 5
+app.post("/app/roll/" (req, res) => {
+  const sides = parseInt(req.body.sides)
+  const dice = parseInt(req.body.dice)
+  const rolls = parseInt(req.body.rolls)
+  
+  res.send(roll(sides, dice, rolls))
+});
+
+// part 6
